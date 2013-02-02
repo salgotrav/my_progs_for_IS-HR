926a86762663df02f10c56f8a269173dc67624d5
vishal <vishal@vishal-pc.(none)>
2013-02-03 04:42:53 +0530

    Replaced stl components by basic components

--- triquery.cpp
+++ triquery.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include<utility>
-#include<vector>
+//#include<utility>
+//#include<vector>
 #include<cstdio>
 
 using namespace std;
@@ -13,20 +13,27 @@ int main()
   //cin>>num_points>>queries;
   scanf("%d%d",&num_points,&queries);
 
-  pair<int, int> xy; //coordinates of point
-  vector< pair<int,int> > array_points;
+  //pair<int, int> xy; //coordinates of point
+  struct xy_pair {
+    int first;
+    int second;
+  }xy;
+
+  //vector< pair<int,int> > array_points;
+  struct xy_pair *array_points = new xy_pair[num_points];
 
   for (int i=0; i<num_points; i++) {
     //cin>>xy.first>>xy.second;
     scanf("%d%d",&xy.first,&xy.second);
-    array_points.push_back(xy);
+    //array_points.push_back(xy);
+    array_points[i]=xy;
   }//end of for loop 
 
 #ifdef DEBUG_MODE
-  for (vector< pair<int,int> >::iterator iter=array_points.begin(); iter!=array_points.end(); iter++) {
-    xy=*iter;
-    cout<<xy.first<<"\t"<<xy.second<<"\n";
-   }
+  for (int i=0; i<num_points; i++) {
+  //xy=*iter;
+  cout<<array_points[i].first<<"\t"<<array_points[i].second<<"\n";
+  }
 #endif
 
   int x,y,d, xd,yd, count; //xd is x+d
@@ -38,22 +45,24 @@ int main()
     yd=y+d;
     count=0;
 
-    for (vector< pair<int,int> >::iterator iter=array_points.begin(); iter!=array_points.end(); iter++) {
-      xy=*iter;
-      if(xy.first<x)
+    // for (vector< pair<int,int> >::iterator iter=array_points.begin(); iter!=array_points.end(); iter++) {
+    for(int i=0;i<num_points;i++) {
+      //xy=*iter;
+      if(array_points[i].first<x)
 	continue;
-      else if(xy.first>xd)
+      else if(array_points[i].first>xd)
 	continue;
-      else if(xy.second<y)
+      else if(array_points[i].second<y)
 	continue;
-      else if(xy.second>yd)
+      else if(array_points[i].second>yd)
 	continue;
       else {
-	if( (xy.second-y)*(-d)-(xy.first-x-d)*d >=0 )
+	if( (array_points[i].second-y)*(-d)-(array_points[i].first-x-d)*d >=0 )
 	  count++;
 	}//end of last else of ifelse ladder
     }//end of for loop
     //cout<<count<<endl;
     printf("%d\n",count);
   }//end of queries while loop
+  delete[] array_points;
 }//end of main
