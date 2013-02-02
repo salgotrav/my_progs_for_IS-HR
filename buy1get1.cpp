43f1e3d85650a82e6c3571d0a7109171613e726f
vishal <vishal@vishal-pc.(none)>
2013-02-02 14:36:38 +0530

    buy1get1 successfull

--- buy1get1.cpp
+++ buy1get1.cpp
@@ -1,8 +1,44 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
 
 using namespace std;
 
+//#define DEBUG_MODE
+
 int main()
 {
+  int num_test, count, str_len;
+  cin>>num_test;
 
+  string str;
+  //string::iterator iter;
+
+  while(num_test--) {
+    int i=0;
+    count=0;
+    cin>>str;
+
+    sort(str.begin(), str.end());
+#ifdef DEBUG_MODE
+    cout<<str<<endl;
+#endif
+    str_len=str.length();
+
+    while(i<str_len){
+      int diff=str.find_last_of(str[i]) - i;
+      i=i+diff+1;
+      if(diff>0){ //multiple occurences
+	//if(diff%2==0)//odd
+	  count += diff/2+1;
+	//else 
+	//count += diff/2;
+      }
+      else//single occurrence
+	count++;
+    }//end of inner while loop
+    cout<<count<<endl;
+    count=0;
+  }//end of while
+  return 0;
 }//end of main
